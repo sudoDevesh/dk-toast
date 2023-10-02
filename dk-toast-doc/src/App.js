66f3dd813f2f3e8f59c94b813c33eae869ae06ad
Devesh Korde <iamdeveshkorde@gmail.com>
2023-10-02 19:19:14 +0530

    Package intialization

---
+++ dk-toast-doc/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Toast from "dk-toast"
+
+function App() {
+  return (
+    <div className="App">
+      <Toast />
+    </div>
+  );
+}
+
+export default App;
